9244de19a03b1b5fbf33e851ecb10adcb5859fc0
Ryadav8337 <Ryadav8337@conestogac.on.ca>
2024-05-23 19:00:37 -0400

    basic navigation

--- app/src/main/java/com/example/techfreelancers/signUpPage.java
+++ app/src/main/java/com/example/techfreelancers/signUpPage.java
@@ -1,6 +1,9 @@
 package com.example.techfreelancers;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,11 @@ public class signUpPage extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(View->{
+            Intent intent= new Intent(signUpPage.this,login.class);
+            startActivity(intent);
         });
     }
 }
