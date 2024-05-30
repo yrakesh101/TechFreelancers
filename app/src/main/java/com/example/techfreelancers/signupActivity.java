c1c60fa603d6954582e5409bd54d55ec5c98a596
Ryadav8337 <Ryadav8337@conestogac.on.ca>
2024-05-30 18:02:00 -0400

    update background color

--- app/src/main/java/com/example/techfreelancers/signupActivity.java
+++ app/src/main/java/com/example/techfreelancers/signupActivity.java
@@ -2,7 +2,9 @@ package com.example.techfreelancers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +12,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class signupActivity extends AppCompatActivity {
+import com.example.techfreelancers.databinding.ActivitySignUpBinding;
+
+public class signupActivity extends welcomeActivity {
+    ActivitySignUpBinding signUpBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Button loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(View->{
-            Intent intent= new Intent(signupActivity.this, loginActivity.class);
-            startActivity(intent);
+        signUpBinding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(signUpBinding.getRoot());
+        setVariable();
+    }
+
+    private void setVariable() {
+        signUpBinding.signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= signUpBinding.emailEditText.getText().toString();
+                String password=signUpBinding.passwordEditText.getText().toString();
+
+                if (password.length()<6){
+                    Toast.makeText(signupActivity.this, "Password is too short", Toast.LENGTH_SHORT).show();
+                }
+
+            }
         });
     }
 }
